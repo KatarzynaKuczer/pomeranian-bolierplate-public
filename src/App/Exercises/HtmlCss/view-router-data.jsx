d7a364b20cad08a93c05929f7fd312234632a802
Kasia K <katarzyna.kuczer@pomeranianstartit.p>
2023-08-20 16:58:35 +0200

    added basic information about cascads and inheritance

--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -2,5 +2,10 @@ import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 
 import { blockRouterMetaData as blockRouterMetaData121220231 } from './Exercise-example-12-12-2023-1/router-data';
 
-export const blockRouterMetaData = [blockRouterMetaData121220231];
+import { blockRouterMetaData as CSSCasscadeslockRouterMetaData } from './SelectorsAndCascade/router-data';
+
+export const blockRouterMetaData = [
+  blockRouterMetaData121220231,
+  CSSCasscadeslockRouterMetaData,
+];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
