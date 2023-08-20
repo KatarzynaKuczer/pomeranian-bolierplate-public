b35bd6f7b45f324e852f2482502c6a9ff4af16b8
Kasia K <katarzyna.kuczer@pomeranianstartit.p>
2023-08-20 15:34:36 +0200

    added some files

--- src/App/Exercises/ReactExercises/ReactIfStatementsMoreOrLess/ReactIfStatementsMoreOrLess.jsx
+++ src/App/Exercises/ReactExercises/ReactIfStatementsMoreOrLess/ReactIfStatementsMoreOrLess.jsx
@@ -12,9 +12,14 @@ export function ReactIfStatementsMoreOrLess() {
   return (
     <div>
       <input type="number" onChange={handleChange} value={inputValue} />
-      {/* {inputValue < 10 ? 'Warunek spełniony' : 'Warunek nie jest spełniony'} */}
+      {/* 1. sposób na if wewntąrz JSX */}
+      {inputValue < 10 ? 'Warunek spełniony' : 'Warunek nie spełniony'}
+
+      {/* 2. sposób - conditional rendering */}
       {inputValue < 10 && 'Warunek spełniony'}
       {inputValue >= 10 && 'Warunek nie spełniony'}
+
+      {/* Przykładowy conditional rendering dla widoczności elementu */}
       {visible && 'Wyrenderuj mnie jeżeli visible jest true'}
     </div>
   );
