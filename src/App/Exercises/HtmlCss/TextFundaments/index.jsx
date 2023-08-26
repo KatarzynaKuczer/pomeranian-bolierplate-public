9917ebf43b12c7c01bb200e9436e4223e4d79c04
Kasia K <katarzyna.kuczer@pomeranianstartit.p>
2023-08-26 19:41:33 +0200

    text fundaments stylowanie

--- src/App/Exercises/HtmlCss/TextFundaments/index.jsx
+++ src/App/Exercises/HtmlCss/TextFundaments/index.jsx
@@ -1,5 +1,12 @@
 import './styles.css';
 
 export function TextFundaments() {
-  return <p>Ćwiczenia z HTML & CSS - 1</p>;
+  return (
+    <p className="test">
+      Lorem ipsum dolor <span>ble ble</span>sit amet consectetur adipisicing
+      elit. Dolore nostrum id impedit praesentium, aut, officiis repellat quod
+      incidunt optio ad ratione laboriosam excepturi corporis quas, suscipit
+      minima cum voluptates mollitia.
+    </p>
+  );
 }
